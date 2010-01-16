a5c15a5fe11d54d30db45e684d4e7ba5c3011970
Joseph Adams <whitegolem@googlemail.com>
2010-01-16 17:08:24 +0100

    removed test input

--- datetime.py
+++ datetime.py
@@ -1,9 +1,9 @@
 import datetime,sys
-#t=datetime.datetime.now()
-#th=t.hour
-#print t.minute,"minutes past",t.hour,"o'clock"
-print "Please enter the hour to test:"
-th=int(sys.stdin.readline())
+t=datetime.datetime.now()
+th=t.hour
+print t.minute,"minutes past",t.hour,"o'clock"
+#print "Please enter the hour to test:"
+#th=int(sys.stdin.readline())
 if th < 12:
   print "Morning"
 elif th < 17:
