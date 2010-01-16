0b6028d0c10298d46318db3e803f01001e6035f1
Joseph Adams <whitegolem@googlemail.com>
2010-01-16 17:03:37 +0100

    add in basic program

---
+++ datetime.py
@@ -0,0 +1,15 @@
+import datetime,sys
+#t=datetime.datetime.now()
+#th=t.hour
+#print t.minute,"minutes past",t.hour,"o'clock"
+print "Please enter the hour to test:"
+th=int(sys.stdin.readline())
+if th < 12:
+  print "Morning"
+elif th < 17:
+  print "Afternoon"
+elif th < 19:
+  print "Evening"
+else: 
+  print "Night"
+  
